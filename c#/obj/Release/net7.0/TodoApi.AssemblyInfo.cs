209b1881cd4871eebd819c3581ab5e4584abcb4d
saraGedalia <sara023025@gmail.com>
2024-04-05 12:01:14 +0300

    add dockerfile

--- c#/obj/Release/net7.0/TodoApi.AssemblyInfo.cs
+++ c#/obj/Release/net7.0/TodoApi.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4ea93604-c3aa-4bc6-b0af-695bebdf1ec0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
